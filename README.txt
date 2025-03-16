44de8389097c2ff0919dd9b3321fcec0f5633772
Pravin8788 <pravinpansare8788@gmail.com>
2025-03-17 01:00:09 +0530

    Internship Project OnlinewQuizz WebApp

---
+++ README.txt
@@ -0,0 +1,22 @@
+####  Make Sure PHP Version 5.5 to 7.2  #####
+
+Xamp PHP 5.6 download link -  https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/5.6.39/xampp-win32-5.6.39-0-VC11-installer.exe/download
+
+Xamp PHP 5.5 download link -  https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/5.5.33/xampp-win32-5.5.33-0-VC11-installer.exe/download
+
+
+###########     Installation Steps    ##########
+
+1. Download zip file and Unzip file on your local server.
+2. Put this file inside "c:/xampp/htdocs/".
+3. Database Configuration
+	i)   Open phpmyadmin
+	ii)  Create Database named exam
+	iii) Import database exam.sql from downloaded folder(inside Online-Quiz folder)
+4. Open Your browser paste the link  "http://localhost/Project Folder Name/" (ie, "http://localhost/Online-Quiz/")
+
+ADMIN login link :  http://localhost/Online-Quiz/admin.php
+
+admin email : suryaprasadtripathy8@gmail.com
+admin  password : pinkylove
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Quiz System |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/OnlineTest.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro" style="
+            background-color: rgba(0, 0, 0, 0.7);
+            padding:20px;
+            
+            ">
+                <h1> online quiz system </h1>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+            </div>
+        </center>
+    </body>
+</html>
